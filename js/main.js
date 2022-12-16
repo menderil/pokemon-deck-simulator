8ecc0030a8c60adf6d1e61b132bee59c92228561
menderil <constance.weightman@gmail.com>
2022-12-16 15:55:08 -0800

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// build a deck, save to local storage
+// standard only?
+// search for card, add to array. Limit of 60 cards, 4 of each card except energy
+
+// test saved deck
+// list of decks by name, select one to use that value in the js methods
+
+// draw a starting hand
+// use chosen deck, shuffle array, 'draw' 7 cards
+// if no basic mons, reshuffle and draw again
+
+// set prizes
+// 'draw' 6 more cards
