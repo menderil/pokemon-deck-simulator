e604a3fe9130e0cb0846a947c8d3231cc3ad4c3a
menderil <constance.weightman@gmail.com>
2022-12-30 20:09:56 -0800

    Added card search for the "Build" section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,42 @@
+//event listener for deck building search button
+document.querySelector('#searchForCard').addEventListener('click', searchForCard)
+
+//deck build search result ul
+const buildSearchResult = document.querySelector('#searchResult')
+
+function searchForCard(){
+    //grabs the card name
+    let buildSearchInput = document.querySelector('#searchCardText').value;
+    //add quotes for cards with multiple words
+    buildSearchInput = `"${buildSearchInput}"`;
+    console.log(buildSearchInput);
+
+    //clears previous search results
+    while(buildSearchResult.firstChild ){
+        buildSearchResult.removeChild(buildSearchResult.firstChild );
+    }
+
+    fetch(`https://api.pokemontcg.io/v2/cards?q=name:${buildSearchInput}`)
+        .then(response => response.json())
+        .then(searchResponse => {
+            //testing log
+            console.log(searchResponse)
+            
+            //loop through result array
+            for(let i = 0; i < searchResponse.data.length; i++){
+                //create li and img
+                const li = document.createElement('li');
+                const img = document.createElement('img');
+                //set img src to search result image
+                img.src = searchResponse.data[i].images.small;
+                li.appendChild(img);
+                //append to the search result list
+                buildSearchResult.appendChild(li);
+            }
+        });
+
+}
+
 // build a deck, save to local storage
 // standard only?
 // search for card, add to array. Limit of 60 cards, 4 of each card except energy
