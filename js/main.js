c8954afdfea3faa9e5ba74787774ef2eb998f32b
menderil <constance.weightman@gmail.com>
2023-02-09 19:07:29 -0800

    Cleaned up the card adding function, got rid of the extra fetch call, made the comments clearer and easier to read

--- js/main.js
+++ js/main.js
@@ -14,81 +14,66 @@ const currentDeck = [];
 //the array result from the current search
 let currentSearch = [];
 
+/*
+    this function searches for cards using the term entered by the user.
+    it clears the results of the previous search from the buildSearchResult ul.
+    then it takes the value from the input element, converts it into a usable format and sends it to the api.
+    the response data is saved in the searchResponse variable.
+    it loops throught searchResponse, creating an li with card's image url and id and appends the li to the buildSearchResult ul in the html
+*/ 
 function searchForCard(){
-    //grabs the card name
     let buildSearchInput = document.querySelector('#searchCardText').value;
-    //add quotes for cards with multiple words
+    
     buildSearchInput = `"${buildSearchInput}"`;
     console.log(buildSearchInput);
-
 
     clearUL(buildSearchResult);
 
     fetch(`https://api.pokemontcg.io/v2/cards?q=name:${buildSearchInput}`)
         .then(response => response.json())
         .then(searchResponse => {
-            let currentSearch = searchResponse.data;
+            currentSearch = searchResponse.data;
             //testing log
-            console.log(currentSearch)
+            //console.log(currentSearch)
             
             for(let i = 0; i < searchResponse.data.length; i++){
-                buildSearchResult.appendChild(createCardElements(currentSearch[i]));
+                buildSearchResult.appendChild(createCardElements(currentSearch[i], i));
             }
         });
 }
 
-//event listener for clicked card
+/*
+    function runs when the card id under a search result is clicked.
+    it checks that there's room in the deck for a new card to be added, then confirms the user wants to add the card.
+    if they select yes, it checks if the card exists.
+    if it does, and less than the maximum of 4 are in the deck, it adds 1 to the card quantity and total number of cards.
+    if it doesn't exist, it creates a new Card object, adds it to the deck array and adds 1 to the total number of cards.
+*/
 function addCardToDeck(){
-    if(confirm(`Add ${this.innerText} to the deck?`)){
-        //gets the response from the specific id
-        fetch(`https://api.pokemontcg.io/v2/cards?q=id:${this.innerText}`)
-            .then(response => response.json())
-            .then(searchResponse => {
-                //add the card info to deck list
-                //check number of cards in deck is less than 60
-                if (numberOfCards <= 59){
-                    //check if the card is already in the deck
-                    const i = currentDeck.findIndex(e => e.id === searchResponse.data[0].id);
-                    if (i > -1 ) {
-                        //card exists at index "i"
-                        //and there are less than 4 copies
-                        if(currentDeck[i].quantity < 4){
-                            if(confirm(`Card exists in deck. qty: ${currentDeck[i].quantity}. Add another copy?`)){
-                                currentDeck[i].quantity++;
-                                numberOfCards++;
-                                alert(`Quantity is now ${currentDeck[i].quantity}`)
-                            }else{
-                                alert('Card not added');
-                            }
-                        }else{
-                            alert('Quantity 4 of each card is the maximum')
-                        }
-                    }else{
-                        //not in the deck
-                        alert('Card added')
-                        //creats an object with the card info, sets qty to 1
-                        let card = {
-                            id: searchResponse.data[0].id,
-                            name: searchResponse.data[0].name,
-                            img: searchResponse.data[0].images.small,
-                            quantity: 1
-                        }
-                        //adds the card
-                        currentDeck.push(card);
-                        numberOfCards++;
-                    }
-                }else{
-                    alert('The deck is full!')
-                }
-            });
+    if(numberOfCards == 60){
+        alert('The deck is full!');
     }else{
-        alert('Card not added');
+        if(confirm(`Add ${this.innerText} to the deck?`)){
+            const cardToAdd = currentSearch[this.id]
+            const i = currentDeck.findIndex(e => e.id === cardToAdd.id);
+            if (i > -1 ) {
+                currentDeck[i].addCard();
+                numberOfCards++;
+            }else{
+                currentDeck.push(new Card(cardToAdd.name, cardToAdd.images.small, cardToAdd.id));
+                numberOfCards++;
+            }
+        }
     }
 }
-
+/*
+    This function displays the current list of cards in the deck
+    It clears the previous list, if there is one
+    then loops through the deck array, creates an li with the name, id, and quantity of each card
+    then appends the li to the display ul
+*/
 function displayDeckList(){
     clearUL(displayDeckUl);
-
     //loop through current deck list
     for(let i = 0; i < currentDeck.length; i++){
         //create the elements to add
@@ -103,19 +88,22 @@ function displayDeckList(){
     totalCards.innerText = `Total Cards: ${numberOfCards}`;
 }
 
-//this function creates and returns an li element with the card's image and id
-//adds an event listener to the id, allowing it to be added to the deck
-function createCardElements(card){
+/*
+this function creates and returns an li element with the card's image and id
+adds an event listener to the id, allowing it to be added to the deck
+*/
+function createCardElements(card, index){
     const li = document.createElement('li');
     const img = document.createElement('img');
-    const id = document.createElement('span');
+    const cardID = document.createElement('span');
 
     img.src = card.images.small;
-    id.innerText = card.id;
-    id.addEventListener('click', addCardToDeck);
+    cardID.innerText = card.id;
+    cardID.setAttribute("id", index);
+    cardID.addEventListener('click', addCardToDeck);
 
     li.appendChild(img);
-    li.appendChild(id);
+    li.appendChild(cardID);
 
     return li;
 }
@@ -125,5 +113,25 @@ function createCardElements(card){
 function clearUL(list){
     while(list.firstChild ){
         list.removeChild(list.firstChild );
+    }
+}
+
+//class for the card objects to be added to the deck list
+class Card {
+    constructor(name, imageUrl, id) {
+      this.name = name;
+      this.imageUrl = imageUrl;
+      this.id = id;
+      this.quantity = 1;
+    }
+
+    //this function checks how many of this card is already in the deck
+    //if its less than the maximum, it adds 1 card
+    addCard(){
+        if(this.quantity < 4){
+            this.quantity++;
+        }else{
+            alert(`Max quantity of ${this.id} are in the deck`)
+        }  
     }
 }
